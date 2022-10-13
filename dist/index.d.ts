9fe1251a61a53302e28fbff76f40d9d85b1fe630
Viraj Shukla <virajshukla00@gmail.com>
2022-10-14 00:23:02 +0530

    small changes

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const add_commas: (n: number, system: "IND" | "INT") => string;
---
+++ dist/index.modern.js
@@ -0,0 +1,2 @@
+const t=(t,d)=>{const e=t.toString(),r="IND"===d?/\B(?=(\d{2})+.(?!(\d{1})))+(?=(\d{2})+(?!(\d{3})))/g:/\B(?=(\d{3})+(?!\d))/g,c=t%1!=0?e.split("."):e;return t%1!=0?c[0].replace(r,",")+"."+c[1]:e.replace(r,",")};export{t as add_commas};
+//# sourceMappingURL=index.modern.js.map
